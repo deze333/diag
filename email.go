5f101737f018fe020e8a587061a4df11f9fc19e4
Denis Zubkov <deze333@yahoo.com>
2015-03-03 16:14:41 -0800

    added email sender proc

--- email.go
+++ email.go
@@ -1,10 +1,11 @@
 package diag
 
 import (
-    "fmt"
-    "bytes"
-    "strings"
-    "text/template"
+	"bytes"
+	"fmt"
+	"strings"
+	"text/template"
+
 	"github.com/deze333/m8l"
 )
 
@@ -13,9 +14,10 @@ import (
 //------------------------------------------------------------
 
 type EmailNotifier struct {
-    sender map[string]string
-    recipient map[string]string
-    subjectPrefix string
+	sender        map[string]string
+	recipient     map[string]string
+	subjectPrefix string
+	sendProc      func(sender, recipient map[string]string, subj, body string)
 }
 
 var _emailNotifier *EmailNotifier
@@ -25,7 +27,22 @@ var _emailNotifier *EmailNotifier
 //------------------------------------------------------------
 
 func SetEmailNotification(sender, recipient map[string]string, subjPrefix string) {
-    _emailNotifier = &EmailNotifier{sender, recipient, subjPrefix}
+
+	_emailNotifier = &EmailNotifier{
+		sender:        sender,
+		recipient:     recipient,
+		subjectPrefix: subjPrefix,
+	}
+}
+
+func SetEmailNotificationProc(sender, recipient map[string]string, subjPrefix string, sendProc func(sender, recipient map[string]string, subj, body string)) {
+
+	_emailNotifier = &EmailNotifier{
+		sender:        sender,
+		recipient:     recipient,
+		subjectPrefix: subjPrefix,
+		sendProc:      sendProc,
+	}
 }
 
 //------------------------------------------------------------
@@ -33,10 +50,10 @@ func SetEmailNotification(sender, recipient map[string]string, subjPrefix string
 //------------------------------------------------------------
 
 type Email struct {
-    Tag string
-    Subject string
-    ParamsHtml string
-    Params []string
+	Tag        string
+	Subject    string
+	ParamsHtml string
+	Params     []string
 }
 
 // Email template
@@ -64,59 +81,70 @@ var _emailTpl = template.Must(template.New("deze333/diag/email").Parse(`
 //------------------------------------------------------------
 
 func notifyEmail(name, title string, args ...interface{}) {
-    if _emailNotifier == nil {
-        return
-    }
+	if _emailNotifier == nil {
+		return
+	}
 
-    subj := "[" + _emailNotifier.subjectPrefix + "] " + name + " : " + title
-    
-    e := Email{
-        Tag: name,
-        Subject: title,
-        Params: []string{},
-    }
+	subj := "[" + _emailNotifier.subjectPrefix + "] " + name + " : " + title
 
-    // Convert to strings
-    // Change \n to <br> in those who have it
-    ss := make([]string, len(args))
-    for i, v := range args {
-        s := fmt.Sprint(v)
-        if strings.Contains(s, "\n") {
-            ss[i] = strings.Replace(s, "\n", "<br>", -1)
-        } else {
-            ss[i] = s
-        }
-    }
+	e := Email{
+		Tag:     name,
+		Subject: title,
+		Params:  []string{},
+	}
 
-    if len(ss) == 1 {
-        e.Params = append(e.Params, fmt.Sprint(ss[0]))
-    } else {
-        for i := 0; i + 1 < len(ss); i += 2 {
-            key := ss[i]
-            val := strings.Replace(ss[i+1], "\n", "<br>", -1)
-            e.Params = append(e.Params, fmt.Sprintf("<strong>%s</strong> = %s<br>", key, val))
-        }
-    }
+	// Convert to strings
+	// Change \n to <br> in those who have it
+	ss := make([]string, len(args))
+	for i, v := range args {
+		s := fmt.Sprint(v)
+		if strings.Contains(s, "\n") {
+			ss[i] = strings.Replace(s, "\n", "<br>", -1)
+		} else {
+			ss[i] = s
+		}
+	}
 
-    var err error
-    var msg bytes.Buffer
-    err = _emailTpl.Execute(&msg, e)
-    if err != nil {
-        ERROR("diag", "Error generating SOS email via template. Email send aborted.", "err", err)
-        return
-    }
+	if len(ss) == 1 {
+		e.Params = append(e.Params, fmt.Sprint(ss[0]))
+	} else {
+		for i := 0; i+1 < len(ss); i += 2 {
+			key := ss[i]
+			val := strings.Replace(ss[i+1], "\n", "<br>", -1)
+			e.Params = append(e.Params, fmt.Sprintf("<strong>%s</strong> = %s<br>", key, val))
+		}
+	}
 
-    // Create and send email in async mode
-    email := m8l.NewEmail(subj, &msg)
-    email.SetSender(_emailNotifier.sender)
-    email.SetReplyTo(_emailNotifier.recipient["identity"], _emailNotifier.recipient["email"])
-    email.AddTo(_emailNotifier.recipient["identity"], _emailNotifier.recipient["email"])
-    if err = email.Validate(); err != nil {
-        ERROR("diag", "Error validating email. Email send aborted.", "err", err)
-        return
-    }
-    if err = email.SendAsync(); err != nil {
-        ERROR("diag", "Error sending email. Email send aborted.", "err", err)
-        return
-    }
+	var err error
+	var msg bytes.Buffer
+	err = _emailTpl.Execute(&msg, e)
+	if err != nil {
+		ERROR("diag", "Error generating SOS email via template. Email send aborted.", "err", err)
+		return
+	}
+
+	// Async send email
+	if _emailNotifier.sendProc != nil {
+
+		// Via send proc
+		go _emailNotifier.sendProc(
+			_emailNotifier.sender, _emailNotifier.recipient,
+			subj, msg.String())
+
+	} else {
+
+		// Via SMTP
+		email := m8l.NewEmail(subj, &msg)
+		email.SetSender(_emailNotifier.sender)
+		email.SetReplyTo(_emailNotifier.recipient["identity"], _emailNotifier.recipient["email"])
+		email.AddTo(_emailNotifier.recipient["identity"], _emailNotifier.recipient["email"])
+		if err = email.Validate(); err != nil {
+			ERROR("diag", "Error validating email. Email send aborted.", "err", err)
+			return
+		}
+		if err = email.SendAsync(); err != nil {
+			ERROR("diag", "Error sending email. Email send aborted.", "err", err)
+			return
+		}
+	}
 }
